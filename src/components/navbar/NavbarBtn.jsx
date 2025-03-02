dec1df01a0fbc9b2969b2caf08034c14351ba95d
Sarthak Gupta <sarthak.f.gupta@accenture.com>
2025-03-02 15:15:00 +0530

    resume link update

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -5,7 +5,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 const NavbarBtn = () => {
   return (
     <a
-      href="https://drive.google.com/file/d/1_DvCd_O7ZzBDsH4s3k8l-aXG8S0TfBjl/view?usp=sharing"
+      href="https://drive.google.com/file/d/1euIoNvrQ5FINaeqXzhyAQ-6whxgBu7a7/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer"
       className="px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow"
